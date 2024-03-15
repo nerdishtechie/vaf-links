8e55950121fcb8175a0e1c658d6090ac779e0af6
Latheesh <latheesh805@gmail.com>
2024-03-15 09:43:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+function App(){
+  return(
+    <div>
+      <h1 style={{color:'green',textAlign:'center'}}>ITV VAF links for Schengen Nations from India</h1>
+      <div className='links'>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Austria</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://visaonweb.diplomatie.be/' target='_blank' rel='noopener noreferrer'>Belgium</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Croatia</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Czech_Republic</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://applyvisa.um.dk/NVP.App/frontpage' target='_blank' rel='noopener noreferrer'>Denmark</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Estonia</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Finland</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://connect.france-visas.gouv.fr/realms/usager/protocol/openid-connect/auth?response_type=code&client_id=fv-fo-keycloak-web&scope=openid&state=5728cbad-c335-458a-b321-f45edb31893c&redirect_uri=http://application-form.france-visas.gouv.fr/fv-fo-dde/login/oauth2/code/keycloak&nonce=w4MR0BnFocbCKKeCwu1YbvTAtCBH_EbqYBZXK404rFY' target='_blank' rel='noopener noreferrer'>France</a>
+      
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Germany</a>
+      <br></br>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://in-gr-services.gvcworld.eu/login/visa-request' target='_blank' rel='noopener noreferrer'>Greece</a>
+      
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Hungary</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://visa.government.is/' target='_blank' rel='noopener noreferrer'>Iceland</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Italy</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://epak.pmlp.gov.lv/NVIS.EService001.WebSite/ApplicationMain.aspx' target='_blank' rel='noopener noreferrer'>Latvia</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Liechtenstein</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://visa.vrm.lt/epm/' target='_blank' rel='noopener noreferrer'>Lithuania</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Luxembourg</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Malta</a>
+      <br></br>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Netherlands</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Norway</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='https://row7.vfsglobal.com/OnlineVAF-Poland/?Country=nS+8hVbxHEpQ8sFyvMtLIg==' target='_blank' rel='noopener noreferrer'>Poland</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Portugal</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Slovakia</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Slovenia</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Spain</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Sweden</a>
+      <a style={{fontSize:'30px',margin:'15px'}} href='' target='_blank' rel='noopener noreferrer'>Switzerland</a>
+      
+      
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+    </div>
+  );
+}
+export default App;
